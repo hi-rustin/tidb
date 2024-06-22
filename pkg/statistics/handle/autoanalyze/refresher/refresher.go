6178d5beb9651b912535e7caa1723c84290e6ded
hi-rustin <rustin.liu@gmail.com>
2024-06-23 01:06:21 +0800

    fix: send all

--- pkg/statistics/handle/autoanalyze/refresher/refresher.go
+++ pkg/statistics/handle/autoanalyze/refresher/refresher.go
@@ -78,6 +78,12 @@ func (r *Refresher) PickOneTableAndAnalyzeByPriority() bool {
 	if !r.autoAnalysisTimeWindow.isWithinTimeWindow(time.Now()) {
 		return false
 	}
+	if r.Jobs.Len() == 0 {
+		statslogutil.StatsLogger().Info(
+			"No table to analyze",
+		)
+		return false
+	}
 
 	se, err := r.statsHandle.SPool().Get()
 	if err != nil {
@@ -122,13 +128,8 @@ func (r *Refresher) PickOneTableAndAnalyzeByPriority() bool {
 			)
 			break
 		}
-		// Only analyze one table each time.
-		return true
 	}
-	statslogutil.SingletonStatsSamplerLogger().Info(
-		"No table to analyze",
-	)
-	return false
+	return true
 }
 
 // RebuildTableAnalysisJobQueue rebuilds the priority queue of analysis jobs.
